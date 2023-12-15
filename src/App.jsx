83fcf51ef5bf8c622148ae624860a065297cc592
faizan-rauf05 <faizanrauf2525@gmail.com>
2023-12-16 01:08:57 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Board from './Board'
+
+const App = () => {
+    return (
+        <>
+            <div className="main-area">
+                <Board />
+            </div>
+        </>
+    )
+}
+
+export default App
